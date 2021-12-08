cb62a8d6160c9e647da6f5d2e68ecf201039e94c
agustinm18 <71091200+agustinm18@users.noreply.github.com>
2021-12-08 18:35:19 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRAUHhZIcPvlHv_fQIS4O9tlSyADN200Y",
+  authDomain: "storyapp-3eccc.firebaseapp.com",
+  databaseURL: "https://storyapp-3eccc-default-rtdb.firebaseio.com",
+  projectId: "storyapp-3eccc",
+  storageBucket: "storyapp-3eccc.appspot.com",
+  messagingSenderId: "607304620906",
+  appId: "1:607304620906:web:fb3bdadb9d34bdc75f6313"
+};
